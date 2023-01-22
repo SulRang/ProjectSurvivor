2f9d0eb6f02e27bad80b18248472f3cffd48f09b
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-01-23 00:41:33 +0900

    Weapon_Aura & arrange unused files
    Scripting of weapon aura. And arrage unused asset files

---
+++ Assets/Scripts/Weaphone/WeaponAura.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAura : MonoBehaviour
+{
+    [SerializeField]
+    float damage = 1f;
+
+    [SerializeField]
+    int level = 1;
+
+    [SerializeField]
+    Player_Move_JS player;
+
+    void Start()
+    {
+        //damage *= Player_Status.instance.DMG;
+        this.gameObject.transform.SetParent(player.transform);
+    }
+
+    void Attack()
+    {
+
+    }
+
+    //테스트용. 실제로는 LevelUp을 사용해야함
+    private void Update()
+    {
+        if (level != 1)
+        {
+            transform.localScale = new Vector2(level * 2f, level * 2f);
+        }
+    }
+
+    void LevelUp()
+    {
+        ++level;
+        transform.localScale = new Vector2(level * 1.5f, level * 1.5f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Monster")
+        {
+            collision.GetComponent<Monster>().GetDamage((int)damage);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Monster")
+        {
+            collision.GetComponent<Monster>().GetDamage((int)damage);
+        }
+    }
+}
